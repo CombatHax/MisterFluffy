60cf04ccd0aa1f19e5df4b481be6cdb9b6a9a978
lunarisntcool <jamesg9612@gmail.com>
2023-01-10 20:32:37 -0500

    did a little teensy bit of styling

--- frontpage/script.js
+++ frontpage/script.js
@@ -1,4 +1,3 @@
 const login = () => {window.location.pathname = "MisterFluffy/signin/index.html"}
-
 const unhoverlogin = () => document.getElementsByClassName("login")[0].style.backgroundColor = "hsl(180, 27%, 48%)"
 const hoverlogin = () => document.getElementsByClassName("login")[0].style.backgroundColor = "hsl(180, 27%, 20%)"
